bc6306fec18b80f05f1066427c89f3d1cee0c351
waleedN001 <waleed.n1001@gmail.com>
2022-03-14 08:56:45 +0500

    Updates

---
+++ precache-manifest.33d6a6fb4b394c22daefb33e8c61201e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8dee8f9388e2b842308",
+    "url": "/texTutils-react/static/css/main.1d9cae2b.chunk.css"
+  },
+  {
+    "revision": "b8dee8f9388e2b842308",
+    "url": "/texTutils-react/static/js/main.b8dee8f9.chunk.js"
+  },
+  {
+    "revision": "db20f86d0530543db744",
+    "url": "/texTutils-react/static/js/1.db20f86d.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/texTutils-react/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "2fcdaf07c3d9b0037123",
+    "url": "/texTutils-react/static/js/runtime~main.2fcdaf07.js"
+  },
+  {
+    "revision": "01259050be86a99047a29abae0e9d42e",
+    "url": "/texTutils-react/index.html"
+  }
+];
